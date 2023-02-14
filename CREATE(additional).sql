03b045d15f5af928fbc56d57c9d45cb2af3f6240
pinocoladium <utkonosnadereve@bk.ru>
2023-02-14 19:20:58 +0500

    first full mod

--- CREATE(additional).sql
+++ CREATE(additional).sql
@@ -6,19 +6,19 @@ CREATE TABLE IF NOT EXISTS Departament (
 CREATE TABLE IF NOT EXISTS Employee (
 	employee_id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
-	departament INTEGER FOREIGN KEY REFERENCES Departament(departament_id)
+	departament INTEGER REFERENCES Departament(departament_id)
 );
 
 CREATE TABLE IF NOT EXISTS Сhief (
 	сhief_id SERIAL PRIMARY KEY,
-	employee INTEGER FOREIGN KEY REFERENCES Employee(employee_id),
-	departament INTEGER FOREIGN KEY REFERENCES Departament(departament_id)
+	employee INTEGER REFERENCES Employee(employee_id),
+	departament INTEGER REFERENCES Departament(departament_id)
 );
 
 CREATE TABLE IF NOT EXISTS Employee_chief (
 	id SERIAL PRIMARY KEY,
-	employee_id INTEGER FOREIGN KEY REFERENCES Employee(employee_id),
-	сhief_id INTEGER FOREIGN KEY REFERENCES Сhief(сhief_id)
+	employee_id INTEGER REFERENCES Employee(employee_id),
+	сhief_id INTEGER REFERENCES Сhief(сhief_id)
 );
 
 
